182c7a0d84cc31b30d32fd9c921cb8774a5a544c
Juan Manuel Montiel <Juan.Montiel@sdgku.edu>
2024-11-07 20:39:39 +1030

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from pages import views
+from .views import (
+    IssueListView,
+    IssueDetailView,
+    IssueCreateView,
+    IssueUpdateView,
+    IssueDeleteView
+)
 
-urlpatterns=[
-    path("", views.HomePageView.as_view(), name = "home"),
-    path("about/", views.AboutPageView.as_view(), name = "about"),
+urlpatterns = [
+    path('', IssueListView.as_view(), name='list'),
+    path('list/', IssueDetailView.as_view(), name='detail'),
+    path('new/', IssueCreateView.as_view(), name='new'),
+    path('<int:pk>/edit/', IssueUpdateView.as_view(), name='edit'),
+    path('<int:pk>/delete/', IssueDeleteView.as_view(), name='delete'),
 ]
--- views.py
+++ views.py
@@ -1,3 +1,108 @@
-from django.shortcuts import render
+from django.views.generic import(
+    ListView,
+    CreateView,
+    DetailView,
+    DeleteView,
+    UpdateView,
+)
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+from django.urls import reverse_lazy
+from .models import Issue, Status
+from accounts.models import CustomUser, Role, Team
 
-# Create your views here.
+
+class IssueListView(LoginRequiredMixin, ListView):
+    template_name = "issues/list.html"
+    model = Issue
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user()
+        role = Role.objects.get(name="product owner")
+        team_po = (
+            CustomUser.objects
+            .filter(team=user.team)
+            .filter(role=role)
+        )
+        to_do = Status.objects.get(name="to do")
+        context["to_do_list"] = (
+            Issue.objects
+            .filter(status=to_do)
+            .filter(reporter=team_po[0])
+            .order_by("created_on").reverse()
+        )
+        in_progress = Status.objects.get(name="in progress")
+        context("in_progress_list") = (
+            Issue.objects
+            .filter(status=in_progress)
+            .filter(reporter=team_po[0])
+            .order_by("created_on").reverse()
+        )
+        done = Status.objects.get(name="done")
+        context["done_list"] = (
+            Issue.objects
+            .filter(status=done)
+            .filter(reporter=team_po[0])
+            .order_by("created_on").reverse()
+        )
+        return context
+    
+class IssueDetailView(LoginRequiredMixin, DetailView):
+    template_name = "issue/detail.html"
+    model = Issue
+
+class IssueCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    template_name = "issue/new.html"
+    model = Issue
+    fields = [
+        "summary", "description", "assignee",
+        "priority", "status"
+    ]
+
+    def test_func(self):
+        role = Role.objects.get(name="owner")
+        return self.request.user.role == role
+
+    def from_valid(self, form):
+        form.instance.reporter = self.request.user
+        return super().form_valid(form)
+
+class IssueUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    template_name = "issue/edit.html"
+    model = Issue
+    fields = [
+        "summary", "description", "assignee",
+        "priority", "status"
+    ]
+
+    def test_func(self):
+        po_role = Role.objects.get(name="product owner")
+        product_owner = (
+            CustomUser.objects
+            .filter(role=po_role)
+            .filter(team=self.request.user.team)
+        )
+        if product_owner:
+            issue = self.get.object()
+            return issue.reporter == product_owner[0]
+        return False
+
+class IssueDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
+    template_name = "issue/delete.html"
+    model = Issue
+    success_url = reverse_lazy("list")
+
+    def test_func(self):
+        po_role = Role.objects.get(name="product owner")
+        product_owner = (
+            CustomUser.objects
+            .filter(role=po_role)
+            .filter(team=self.request.user.team)
+        )
+        if product_owner:
+            issue = self.get.object()
+            return issue.reporter == product_owner[0]
+        return False
